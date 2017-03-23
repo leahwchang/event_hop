8ef23491adbd38d852737d383f1a402958907c31
Leah W. Chang <leahwchang@gmail.com>
2017-03-23 11:12:35 -0400

    change asset pipeline sequence for gmaps

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
 //= require jquery
 //= require jquery_ujs
 //= require jquery.turbolinks
 //= require bootstrap-sprockets
 //= require underscore
-//= require gmaps/google
 //= require turbolinks
 //= require_tree .
